562254f42f6290179d18ce315b28295fdeaba403
Smetskii Ivan <smetskii.ivan@gmail.com>
2021-03-19 15:25:26 +0300

    Task-1 UPDATE

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -11,15 +11,38 @@ public class StatsService {
 
     public int calculateAverageSales(int[] amountOfSales) {
         int sum = 0;
-        int averageSales = 0;
+         int averageSales = 0;
         for (int amount : amountOfSales) {
             sum = sum + amount;
-            averageSales =  sum  / amountOfSales.length;
+            averageSales = sum / amountOfSales.length;
         }
         return averageSales;
     }
 
+    public int calculateMaxSalesMonth(int[] amountOfSales) {
+        int currentMax = amountOfSales[0];
+        for (int amount : amountOfSales) {
+            if (currentMax < amount) {
+                currentMax = amount;
+            }
+        }
+        return currentMax;
+    }
+
+    public int calculateMinSalesMonth(int[] amountOfSales) {
+        int currentMin = amountOfSales[0];
+        for (int amount : amountOfSales) {
+            if (currentMin > amount) {
+                currentMin = amount;
+            }
+        }
+        return currentMin;
+    }
+
+    public int calculateMonthsWithLowestAverageValue(int[] amountOfSales) {
 
 
-
+    }
 }
+
+
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
+  public static int[]  amountOfSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
 
     @Test
     void calculateSum() {
         StatsService service = new StatsService();
-        int[] amountOfSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 180;
         int actual = service.calculateSum(amountOfSales);
         assertEquals(expected, actual);
@@ -19,9 +20,33 @@ class StatsServiceTest {
     @Test
     void calculateAverageSales() {
         StatsService service = new StatsService();
-        int[] amountOfSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 15;
         int actual = service.calculateAverageSales(amountOfSales);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateMaxSalesMonth() {
+        StatsService service = new StatsService();
+        int expected = 20;
+        int actual = service.calculateMaxSalesMonth(amountOfSales);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void calculateMinSalesMonth() {
+        StatsService service = new StatsService();
+        int expected = 7;
+        int actual = service.calculateMinSalesMonth(amountOfSales);
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    void monthsWithLowestAverageValue() {
+        StatsService service = new StatsService();
+        int expected = 5;
+        int actual = service.calculateMonthsWithLowestAverageValue(amountOfSales);
+        assertEquals(expected,actual);
     }
 }
